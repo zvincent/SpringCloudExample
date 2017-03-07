8c68d1839e16e1fee7f2581a6b0a452ddb91d502
zvincent <1361530178@qq.com>
2017-03-07 17:06:58 +0800

    init

---
+++ src/main/java/com/springcloud/example/SpringCloudExampleApplication.java
@@ -0,0 +1,12 @@
+package com.springcloud.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springcloud/example/SpringCloudExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springcloud.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
